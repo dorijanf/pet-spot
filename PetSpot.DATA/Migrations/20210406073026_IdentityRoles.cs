a7082bf058d90c6277df517b5e88b67dcf54115f
GDI\dorijan.fabijanic <dorijan.fabijanic@gdi.net>
2021-04-06 09:32:44 +0200

    added default identity roles

---
+++ PetSpot.DATA/Migrations/20210406073026_IdentityRoles.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PetSpot.DATA.Migrations
+{
+    public partial class IdentityRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1", "f7a62860-7e2a-438c-a502-2c4be9610cde", "Registered user", "registered user" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2", "71980db9-3387-4088-bf7f-e69b3717c73f", "Administrator", "administrator" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+        }
+    }
+}
